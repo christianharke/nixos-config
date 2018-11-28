14751ecf83bdd651a7fa48636f78eeb5f4d5242e
Christian Harke <ch.harke@gmail.com>
2018-11-28 23:54:58 +0100

    Open ports necessary for nfs

--- roles/nfs.nix
+++ roles/nfs.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 
 {
+  networking.firewall.allowedTCPPorts = [ 111 2049 20048 ];
+  networking.firewall.allowedUDPPorts = [ 111 20048 ];
+
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
     /       *(ro,fsid=0)
