903c37eb8c9d691207fc36bb0c81861e40df71b2
Christian Harke <ch.harke@gmail.com>
2020-02-01 15:09:19 +0100

    Declare home office printer in printing module

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,11 +1,38 @@
 { config, pkgs, ...}:
 
 {
+
   services.printing = {
     enable = true;
     drivers = [ pkgs.hplipWithPlugin ];
   };
+
+  hardware = {
+    printers.ensurePrinters = [{
+      name = "pr-hp-chr";
+      location = "Home Office";
+      description = "HP Officejet Pro 8600 Plus";
+      deviceUri = "socket://pr-hp-chr";
+      model = "drv:///hp/hpcups.drv/hp-officejet_pro_8600.ppd";
+      ppdOptions = {
+        "PageSize" = "A4";
+        "Duplex" = "DuplexNoTumble";
+        "InputSlot" = "Tray2";
+        "ColorModel" = "RGB";
+        "MediaType" = "Plain";
+        "OutputMode" = "Normal";
+        "OptionDuplex" = "True";
+      };
+    }];
+
+    sane = {
+      enable = true;
+      extraBackends = [ pkgs.hplipWithPlugin ];
+    };
+  };
+
   environment.systemPackages = [
     pkgs.unpaper
   ];
+
 }
--- modules/user.nix
+++ modules/user.nix
@@ -18,7 +18,7 @@ in
     isNormalUser = true;
     uid = 1000;
     extraGroups = [
-      "wheel"
+      "wheel" "scanner"
     ];
     initialPassword = "changeme";
     openssh.authorizedKeys.keyFiles = [ keyFile ];
