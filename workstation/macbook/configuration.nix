a98b4ef637da88a808d5b751e46399e6b5a8a917
Christian Harke <ch.harke@gmail.com>
2020-02-23 17:15:18 +0100

    Add dropbox to macbook

--- workstation/macbook/configuration.nix
+++ workstation/macbook/configuration.nix
@@ -29,7 +29,11 @@
 
   software = {
     gaming = [];
+    extra = with pkgs; [
+      dropbox-cli
+    ];
   };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
