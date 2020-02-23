9095a98a82ff4adffcc0bdd18995ec68d5f87bd8
Christian Harke <ch.harke@gmail.com>
2020-02-23 17:14:59 +0100

    Remove gaming stuff from macbook

--- workstation/macbook/configuration.nix
+++ workstation/macbook/configuration.nix
@@ -27,6 +27,9 @@
 
   services.xserver.xkbOptions = "caps:swapescape";
 
+  software = {
+    gaming = [];
+  };
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
