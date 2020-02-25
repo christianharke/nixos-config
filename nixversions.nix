d2e8c44472b5b5c624d85e426edaf257bb75ffcb
Christian Harke <christian.harke@bluecare.ch>
2020-02-25 09:59:38 +0100

    Update nixpkgs1903 hash

--- nixversions.nix
+++ nixversions.nix
@@ -4,7 +4,7 @@ let
 
   nixpkgs1903 = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz";
-    sha256 = "1b3h4mwpi10blzpvgsc0191k4shaw3nw0qd2p82hygbr8vv4g9dv";
+    sha256 = "11z6ajj108fy2q5g8y4higlcaqncrbjm3dnv17pvif6avagw4mcb";
   };
   pkgs1903 = import nixpkgs1903 { config = config; };
 
