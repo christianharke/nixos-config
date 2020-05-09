1afbfae21d83dc48238635231fce195c3413180c
Christian Harke <ch.harke@gmail.com>
2020-05-09 22:28:41 +0200

    Update intel microcode on rig

--- workstation/rig/hardware/rig.nix
+++ workstation/rig/hardware/rig.nix
@@ -3,7 +3,12 @@
 {
   nix.maxJobs = lib.mkOverride 20 4;
 
-  hardware.opengl.driSupport32Bit = true;
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    enableRedistributableFirmware = true;
+    opengl.driSupport32Bit = true;
+  };
+
   services.xserver.videoDrivers = [
     "nvidia"
   ];
