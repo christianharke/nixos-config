2d8da72353c5d749f59f6025f6f568587c6939e7
Christian Harke <christian.harke@bluecare.ch>
2020-10-17 09:57:07 +0200

    Add openjdk11

--- modules/java.nix
+++ modules/java.nix
@@ -4,6 +4,7 @@ let
 
   v = import ../nixversions.nix {};
   java8 = v.pkgsUnstable.pkgs.openjdk8;
+  java11 = pkgs.openjdk11;
   
 in
 
@@ -11,12 +12,16 @@ in
   environment = {
     systemPackages = [
       java8
+      java11
     ];
 
     variables = {
       JAVA_HOME = "${java8}/lib/openjdk";
+      JAVA8_HOME = "${java8}/lib/openjdk";
+      JAVA11_HOME = "${java11}/lib/openjdk";
       JDK_HOME = "${java8}/lib/openjdk";
+      JDK8_HOME = "${java8}/lib/openjdk";
+      JDK11_HOME = "${java11}/lib/openjdk";
     };
   };
-
 }
