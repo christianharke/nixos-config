cd272e9320b705fd11294a9c3aca42855ea3d3bf
Christian Harke <christian.harke@bluecare.ch>
2020-11-03 08:28:15 +0100

    Enable auto-upgrade

--- modules/common/common.nix
+++ modules/common/common.nix
@@ -88,9 +88,16 @@
     };
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system = {
+    autoUpgrade = {
+      enable = true;
+      allowReboot = false;
+      channel = https://nixos.org/channels/nixos-20.09;
+    };
+    # This value determines the NixOS release with which your system is to be
+    # compatible, in order to avoid breaking some software such as database
+    # servers. You should change this only after NixOS release notes say you
+    # should.
+    stateVersion = "20.03"; # Did you read the comment?
+  };
 }
