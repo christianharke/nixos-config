425c6d1ef16cb510030406163641e31be80223f3
Christian Harke <christian.harke@bluecare.ch>
2021-03-31 21:07:29 +0200

    Use NixOS' default Java version by default

--- modules/java.nix
+++ modules/java.nix
@@ -2,23 +2,25 @@
 
 let
 
-  java8 = pkgs.openjdk8;
-  java11 = pkgs.openjdk11;
+  java = pkgs.jdk;
+  java8 = pkgs.jdk8;
+  java11 = pkgs.jdk11;
   
 in
 
 {
   environment = {
     systemPackages = [
+      java
       java8
       java11
     ];
 
     variables = {
-      JAVA_HOME = "${java8}/lib/openjdk";
+      JAVA_HOME = "${java}/lib/openjdk";
       JAVA8_HOME = "${java8}/lib/openjdk";
       JAVA11_HOME = "${java11}/lib/openjdk";
-      JDK_HOME = "${java8}/lib/openjdk";
+      JDK_HOME = "${java}/lib/openjdk";
       JDK8_HOME = "${java8}/lib/openjdk";
       JDK11_HOME = "${java11}/lib/openjdk";
     };
