9b4805b596aa9640713c18664b986dda9d984ec7
Christian Harke <christian.harke@bluecare.ch>
2021-04-30 23:12:16 +0200

    Add battery check notifications

--- modules/common/desktop.nix
+++ modules/common/desktop.nix
@@ -35,6 +35,34 @@
           ExecStart = "${pkgs.dunst}/bin/dunst";
         };
       };
+
+      battery-check = {
+        description = "Battery level notification";
+        script = ''
+          THRESHOLD=10 # how low is too low?
+          DISCHARGING="$(${pkgs.acpi}/bin/acpi -b | head -n 1 | grep Discharging | wc -l)"
+
+          export DISPLAY=:0
+
+          BATTERY_LEVEL=$(${pkgs.acpi}/bin/acpi  -b | grep -P -o '[0-9]+(?=%)')
+
+          if [ $BATTERY_LEVEL -le $THRESHOLD -a $DISCHARGING = "1" ]
+          then
+              ${pkgs.libnotify}/bin/notify-send -u critical -t 60000 "Battery low" "Battery level is $BATTERY_LEVEL%!"
+          fi
+        '';
+      };
+    };
+
+    timers = {
+      battery-check = {
+        description = "Battery level notification timer";
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          OnBootSec = "1m";
+          OnUnitInactiveSec = "1m";
+        };
+      };
     };
   };
 
