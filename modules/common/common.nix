5096b235b9004d2581854c31b8a404c6113992fa
Christian Harke <christian@harke.ch>
2021-05-13 10:47:29 +0200

    Configure locale settings

--- modules/common/common.nix
+++ modules/common/common.nix
@@ -1,6 +1,24 @@
 { config, pkgs, ... }:
 
+let
+
+  localeLang = "en_US.UTF-8";
+  localeFormats = "de_CH.UTF-8";
+
+in
+
 {
+  i18n = {
+    defaultLocale = localeLang;
+    extraLocaleSettings = {
+      LC_NUMERIC = localeFormats;
+      LC_TIME = localeFormats;
+      LC_MONETARY = localeFormats;
+      LC_PAPER = localeFormats;
+      LC_MEASUREMENT = localeFormats;
+    };
+  };
+
   location = {
     latitude = 47.5;
     longitude = 8.75;
